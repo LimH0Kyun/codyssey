2a82ea12254cecaccc73a638fa9348e7460dcd71
LimH0Kyun <ghrbs1026@m365.dongyang.ac.kr>
2025-03-27 19:39:33 +0900

    w3 main.py update

--- w3/src/main.py
+++ w3/src/main.py
@@ -1,32 +1,29 @@
 import random
-import datetime
 
 
 class DummySensor:
     def __init__(self):
         self.env_values = {
-            "mars_base_internal_temperature": None,
-            "mars_base_external_temperature": None,
-            "mars_base_internal_humidity": None,
-            "mars_base_external_illuminance": None,
-            "mars_base_internal_co2": None,
-            "mars_base_internal_oxygen": None,
+            "mars_base_internal_temperature": 0,
+            "mars_base_external_temperature": 0,
+            "mars_base_internal_humidity": 0,
+            "mars_base_external_illuminance": 0,
+            "mars_base_internal_co2": 0,
+            "mars_base_internal_oxygen": 0,
         }
 
     def set_env(self):
         # 환경 값을 랜덤으로 생성하여 env_values에 저장
-        self.env_values["mars_base_internal_temperature"] = random.uniform(18, 30)
-        self.env_values["mars_base_external_temperature"] = random.uniform(0, 21)
-        self.env_values["mars_base_internal_humidity"] = random.uniform(50, 60)
-        self.env_values["mars_base_external_illuminance"] = random.uniform(500, 715)
-        self.env_values["mars_base_internal_co2"] = random.uniform(0.02, 0.1)
-        self.env_values["mars_base_internal_oxygen"] = random.uniform(4, 7)
+        self.env_values["mars_base_internal_temperature"] = round(random.uniform(18, 30), 2)
+        self.env_values["mars_base_external_temperature"] = round(random.uniform(0, 21), 2)
+        self.env_values["mars_base_internal_humidity"] = round(random.uniform(50, 60), 2)
+        self.env_values["mars_base_external_illuminance"] = round(random.uniform(500, 715), 2)
+        self.env_values["mars_base_internal_co2"] = round(random.uniform(0.02, 0.1), 2)
+        self.env_values["mars_base_internal_oxygen"] = round(random.uniform(4, 7), 2)
 
     def get_env(self):
         # env_values를 반환하고 로그 파일에 기록
-        # 현재 날짜와 시간
-        now = datetime.datetime.now()
-        timestamp = now.strftime("%Y-%m-%d %H:%M:%S")
+        timestamp = "2025-01-01 00:00:00"
 
         # 로그 메시지 생성
         log_message = (
